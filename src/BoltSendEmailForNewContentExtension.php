dc0f107d405bdeeaa99db8579682900b0371e958
Nicolas Béhier-Dévigne <nbehierdevigne@gmail.com>
2016-07-22 13:18:23 +0200

    Create BoltSendEmailForNewContentExtension.php
    With default config

---
+++ src/BoltSendEmailForNewContentExtension.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Bolt\Extension\Leskis\BoltSendEmailForNewContent;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * BoltSendEmailForNewContent extension class.
+ *
+ * @author Nicolas Béhier-Dévigne
+ */
+class BoltSendEmailForNewContentExtension extends SimpleExtension
+{
+    protected function getDefaultConfig()
+    {
+        return [
+            'debug' => [
+                'enabled' => true,
+                'address' => 'noreply@example.com'
+            ],
+
+            'subscribers' => [
+                'contenttype' => 'subscribers',
+                'emailfield'  => 'email'
+            ],
+
+            'templates' => [
+                'emailbody'    => '@BoltSendEmailForNewContent/email.twig',
+                'emailsubject' => '@BoltSendEmailForNewContent/_subject.twig'
+            ],
+
+            'notifications' => [
+                'entries' => [
+                    'enabled'     => true,
+                    'event'       => 'new-pusblished',
+                    'subscribers' => [
+                        'contenttype' => 'subscribers',
+                        'emailfield'  => 'email',
+                        'filter'      => [
+                            'field' => 'newcontentsubscription',
+                            'value' => true
+                        ],
+                    ],
+                    'debug' =>   true,
+                    'email' => [
+                        'subject'       => 'New entry published',
+                        'from_name'     => '',
+                        'from_email'    => '',
+                        'replyto_name'  => '',
+                        'replyto_email' => ''
+                    ],
+                    'templates' => [
+                        'emailbody'    => '@BoltSendEmailForNewContent/email.twig',
+                        'emailsubject' => '@BoltSendEmailForNewContent/_subject.twig'
+                    ],
+                ],
+            ]
+        ];
+    }
+}
