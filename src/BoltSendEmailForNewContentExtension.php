d2eda15781de76a94d3c5c445770297d292b5a22
Nicolas Béhier-Dévigne <nbehierdevigne@gmail.com>
2016-07-26 09:17:36 +0200

    Check and Get subscribers + Test when entity newly published

--- src/BoltSendEmailForNewContentExtension.php
+++ src/BoltSendEmailForNewContentExtension.php
@@ -20,55 +20,103 @@ class BoltSendEmailForNewContentExtension extends SimpleExtension
      */
     protected function subscribe(EventDispatcherInterface $dispatcher)
     {
-        // Post-save hook for topic and reply creations
+        // Pre-save hook
         $dispatcher->addListener(StorageEvents::PRE_SAVE, [$this, 'hookPreSave']);
     }
-    
+
     /**
-     * Post-save hook for topic and reply creations
-     *
+     * Pre-save hook
      * @param \Bolt\Events\StorageEvent $event
      */
     public function hookPreSave(StorageEvent $event)
     {
         // Get contenttype
-        $contenttype = $event->getContentType();
+        $contenttype                = $event->getContentType();
         $aNotificationsContentTypes = $this->getNotifContentTypes();
-        if (empty($contenttype) || empty($aNotificationsContentTypes) || !in_array($contenttype, $aNotificationsContentTypes) ) {
+        if (   empty($contenttype)
+            || empty($aNotificationsContentTypes)
+            || !in_array($contenttype, $aNotificationsContentTypes) ) {
             return;
         }
-    
+
         // Get the record
         $record = $event->getContent();
-        
+
         // Test if newly published
-        // @todo create and published
-        if (false) {
-            // Launch the notification
-            $notify = new Notifications($this->app, $record);
-            
-            // Search subscribers
-            // @todo
-            
-            // Send email foreach subscriber
-            // @todo
-            $notify->doNotification();
+        $contentNewlyPublished = false;
+        if (   $event->isCreate()
+            && array_key_exists('status', $record->values)
+            && $record->values['status'] == 'published') {
+            $contentNewlyPublished = true;
         }
-        else {
+        else if ($record->values['status'] == 'published') {
             // @todo : check if notification already sent
             // use a temporary file or a db table ?
+            $repo = $app['storage']->getRepository($contenttype);
+            $oldRecord = $repo->find($record->id);
+            if (   !empty($oldRecord)
+                && $oldRecord->values['status'] != 'published') {
+                $contentNewlyPublished = true;
+            }
         }
+
+        if ($contentNewlyPublished) {
+            // Launch the notification
+            $notify = new Notifications($this->app, $record);
+
+            // Search subscribers
+            try {
+                $aSubscribers = $this->getSubscribers();
+
+                // Send email foreach subscriber
+                // @todo
+                //$notify->doNotification();
+            } catch (\Exception $e) {
+                //$this->app['logger.system']->info("Notifications to {$recipient['displayName']} <{$recipient['email']}>", ['event' => 'extensions']);
+                return;
+            }
+        }
+
+        return;
     }
-    
+
+    /**
+     * Get Subscribers for notifications
+     * @throws Exception
+     * @return array records
+     */
+    private function getSubscribers()
+    {
+        $config       = $this->getConfig();
+        $entityName   = $config['subscribers']['contenttype'];
+        $fieldName    = $config['subscribers']['emailfield'];
+        $aSubscribers = false;
+
+        try {
+            $meta = $app['storage.metadata']->getClassMetadata($entityName);//'Bolt\Storage\Entity\Users');
+            if (array_key_exists($fieldName, $meta['fields'])
+                && $meta['fields'][$fieldName]['type'] == 'string' ) {
+                $repo = $app['storage']->getRepository($entityName);
+                $aSubscribers = $app['storage']->fetchAll();
+            }
+        } catch (\Exception $e) {
+            throw new \Exception(sprintf("No Subscribers table like %s with email field %s, as defined on config", $entityName, $fieldName), 1);
+        }
+
+        return $aSubscribers;
+    }
+
     /**
      * Get ContentTypes which have subscriptions
+     * @return array
      */
     private function getNotifContentTypes()
     {
-        $config = $this->getConfig();
-        // @todo
+        $config        = $this->getConfig();
+        $aContentTypes = array_keys($config['notifications']);
+        return $aNotificationsContentTypes;
     }
-    
+
     /**
      * {@inheritdoc}
      */
@@ -128,7 +176,7 @@ class BoltSendEmailForNewContentExtension extends SimpleExtension
             ]
         ];
     }
-    
+
     /**
      * {@inheritdoc}
      */
